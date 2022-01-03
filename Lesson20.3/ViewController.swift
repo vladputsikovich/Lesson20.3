019bfb8363d1ea59ccdf8b6920a671c0a5d93cdf
vladputsikovich <vladputsikovich@gmail.com>
2022-01-03 16:12:45 +0300

    Initial Commit

---
+++ Lesson20.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson20.3
+//
+//  Created by Владислав Пуцыкович on 3.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
